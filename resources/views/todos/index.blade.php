29466a0ada5c256d0d4937a8a6a1fb07586239ec
Айдос Тажиниязов <a.tazhiniazov@gmail.com>
2024-03-02 16:24:30 +0500

    Implemented todo list view with actions in a card layout

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,14 +1,70 @@
 @extends('layouts.app')
 
+@section('styles')
+    <style>
+        #outer
+        {
+            width: auto;
+            text-align: center;
+        }
+        .inner
+        {
+            display: inline-block;
+        }
+    </style>
+@endsection
+
 @section('content')
-    <div class="container">
+    <div class="container-fluid">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
+                    <div class="card-header">{{ __('Список задач') }}</div>
+
+                    @if(\Illuminate\Support\Facades\Session::has('alert-success'))
+                        <div class="alert alert-success" role="alert">
+                            {{ \Illuminate\Support\Facades\Session::get('alert-success') }}
+                        </div>
+                    @endif
 
                     <div class="card-body">
-                        Todos index
+                        @if(count($todos) > 0)
+                            <table class="table">
+                                <thead>
+                                <tr>
+                                    <th>Заголовок</th>
+                                    <th>Описание</th>
+                                    <th>Выполнено</th>
+                                    <th>Действии</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($todos as $todo)
+                                    <tr>
+                                        <td>{{ $todo->title }}</td>
+                                        <td>{{ $todo->description }}</td>
+                                        <td>
+                                            @if ($todo->is_completed == 1)
+                                                <a class="btn btn-sm btn-success" href="">Выполнено</a>
+                                            @else
+                                                <a class="btn btn-sm btn-danger" href="">Не выполнено</a>
+                                            @endif
+                                        </td>
+                                        <td id="outer">
+                                            <a class="inner btn btn-sm btn-success" href="">Подробнее</a>
+                                            <a class="inner btn btn-sm btn-info" href="">Изменить</a>
+                                            <form class="d-inline-block" action="">
+                                                <input type="hidden" name="todo_id" value="{{ $todo->id }}">
+                                                <input type="submit" class="btn btn-sm btn-danger" value="Удалить">
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        @else
+                            <h4 class="text-danger">Пока нет задач</h4>
+                        @endif
                     </div>
                 </div>
             </div>
