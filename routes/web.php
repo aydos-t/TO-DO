2a33e7f2033f10e75998bf84bdfb9cb052a5cd54
Айдос Тажиниязов <a.tazhiniazov@gmail.com>
2024-03-02 20:22:51 +0500

    Add route for showing individual todo details in TodoController.

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,4 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('todos/index', [TodoController::class, 'index'])->name('todos.index');
 Route::get('todos/create', [TodoController::class, 'create'])->name('todos.create');
 Route::post('todos/store', [TodoController::class, 'store'])->name('todos.store');
+Route::get('todos/show/{id}', [TodoController::class, 'show'])->name('todos.show');
