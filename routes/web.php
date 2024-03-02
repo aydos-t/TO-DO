227c93bdc8e51752d5f2245c46d6ed6444881aef
Айдос Тажиниязов <a.tazhiniazov@gmail.com>
2024-03-02 11:27:54 +0500

    Add 'index.blade' and an 'index' route that renders the home screen of the site.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('todos/index', [TodoController::class, 'index'])->name('todos.index');
