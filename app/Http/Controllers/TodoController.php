d9323a3c2da3310d45667938ec48cdee0fdd4eeb
Айдос Тажиниязов <a.tazhiniazov@gmail.com>
2024-03-02 16:16:49 +0500

    Added store method to create a new task in TodoController

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,17 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\TodoRequest;
+use App\Models\Todo;
 
 class TodoController extends Controller
 {
     public function index()
     {
-        return view('todos.index');
+        $todos = Todo::all();
+        return view('todos.index', ['todos' => $todos]);
     }
 
     public function create()
     {
         return view('todos.create');
+    }
+
+    public function store(TodoRequest $request)
+    {
+        Todo::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'is_completed' => 0
+        ]);
+
+        $request->session()->flash('alert-success', 'СПИСОК ЗАДАЧ СОЗДАНА УСПЕШНО!');
+        return view('todos.index');
     }
 }
